211dfe09f90222c2a751f9b2dcd5e10ee7a5e6be
Leandro <leandro.almeida@uvvnet.com.br>
2022-06-27 00:53:44 -0300

    detalhando tela e adicionando fonte externa

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
-import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
+import { StatusBar, SafeAreaView, StyleSheet, View } from 'react-native';
 import React from 'react';
 import Cesta from './src/telas/Cesta';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
 
 export default function App() {
+
+  const [fonteCarregata] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "montserratBold": Montserrat_700Bold
+  });
+
+  if (!fonteCarregata) {
+    return <View></View>
+  }
+
   return (
     <SafeAreaView>
       <StatusBar/>
